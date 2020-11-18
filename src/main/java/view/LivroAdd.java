d9d8e429253141c7b12ea15c20b4f0ce2307ee15
Rodrigo Andrade <rdghenrique94@gmail.com>
2020-11-18 16:51:52 -0300

    Area do Funcionario

--- src/main/java/view/LivroAdd.java
+++ src/main/java/view/LivroAdd.java
@@ -42,6 +42,7 @@ public class LivroAdd extends javax.swing.JInternalFrame {
         jButton2 = new javax.swing.JButton();
         jScrollPane3 = new javax.swing.JScrollPane();
         tabelaAddLivro = new javax.swing.JTable();
+        jButton3 = new javax.swing.JButton();
 
         setBorder(javax.swing.BorderFactory.createTitledBorder("Cadastro de Livros"));
         setClosable(true);
@@ -73,15 +74,42 @@ public class LivroAdd extends javax.swing.JInternalFrame {
             new String [] {
                 "LIVRO", "AUTOR", "EDITORA"
             }
-        ));
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        tabelaAddLivro.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tabelaAddLivroMouseClicked(evt);
+            }
+        });
+        tabelaAddLivro.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                tabelaAddLivroKeyReleased(evt);
+            }
+        });
         jScrollPane3.setViewportView(tabelaAddLivro);
+
+        jButton3.setText("Atualizar");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(329, Short.MAX_VALUE)
+                .addContainerGap(34, Short.MAX_VALUE)
+                .addComponent(txtLivro, javax.swing.GroupLayout.PREFERRED_SIZE, 277, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(txtAutor, javax.swing.GroupLayout.PREFERRED_SIZE, 242, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2))
@@ -91,15 +119,17 @@ public class LivroAdd extends javax.swing.JInternalFrame {
                     .addComponent(txtEditora, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(22, 22, 22))
             .addComponent(jScrollPane3)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(356, 356, 356)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(jPanel1Layout.createSequentialGroup()
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                             .addGap(35, 35, 35)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel1)
-                                .addComponent(txtLivro, javax.swing.GroupLayout.PREFERRED_SIZE, 277, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGap(217, 571, Short.MAX_VALUE))
+                            .addComponent(jLabel1)
+                            .addGap(464, 818, Short.MAX_VALUE))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                             .addGap(64, 64, 64)
                             .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -117,21 +147,22 @@ public class LivroAdd extends javax.swing.JInternalFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtAutor, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(txtEditora, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 114, Short.MAX_VALUE)
+                    .addComponent(txtEditora, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtLivro, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(37, 37, 37)
+                .addComponent(jButton3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 56, Short.MAX_VALUE)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 406, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(45, 45, 45))
             .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(jPanel1Layout.createSequentialGroup()
                     .addContainerGap()
                     .addComponent(jLabel1)
-                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                    .addComponent(txtLivro, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(40, 40, 40)
+                    .addGap(82, 82, 82)
                     .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(jButton1)
                         .addComponent(jButton2))
-                    .addContainerGap(501, Short.MAX_VALUE)))
+                    .addContainerGap(503, Short.MAX_VALUE)))
         );
 
         jScrollPane2.setViewportView(jPanel1);
@@ -144,9 +175,7 @@ public class LivroAdd extends javax.swing.JInternalFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 585, Short.MAX_VALUE))
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 596, Short.MAX_VALUE)
         );
 
         pack();
@@ -169,10 +198,47 @@ public class LivroAdd extends javax.swing.JInternalFrame {
         }
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    private void tabelaAddLivroMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tabelaAddLivroMouseClicked
+
+        if (tabelaAddLivro.getSelectedRow() != -1){
+            txtLivro.setText(tabelaAddLivro.getValueAt(tabelaAddLivro.getSelectedRow(), 0).toString());
+            txtAutor.setText(tabelaAddLivro.getValueAt(tabelaAddLivro.getSelectedRow(), 1).toString());
+            txtEditora.setText(tabelaAddLivro.getValueAt(tabelaAddLivro.getSelectedRow(), 2).toString());
+        }else{
+            JOptionPane.showMessageDialog(null, "Selecione Um Livro para Excluir ");
+        }
+
+    }//GEN-LAST:event_tabelaAddLivroMouseClicked
+
+    private void tabelaAddLivroKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_tabelaAddLivroKeyReleased
+         if (tabelaAddLivro.getSelectedRow() != -1){
+            txtLivro.setText(tabelaAddLivro.getValueAt(tabelaAddLivro.getSelectedRow(), 0).toString());
+            txtAutor.setText(tabelaAddLivro.getValueAt(tabelaAddLivro.getSelectedRow(), 1).toString());
+            txtEditora.setText(tabelaAddLivro.getValueAt(tabelaAddLivro.getSelectedRow(), 2).toString());
+        }else{
+            JOptionPane.showMessageDialog(null, "Selecione Um Livro para Editar ");
+        }
+
+
+    }//GEN-LAST:event_tabelaAddLivroKeyReleased
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        
+        if (tabelaAddLivro.getSelectedRow() != -1){
+           tabelaAddLivro.setValueAt(txtLivro.getText(), tabelaAddLivro.getSelectedRow(), 0);
+           tabelaAddLivro.setValueAt(txtAutor.getText(), tabelaAddLivro.getSelectedRow(), 1);
+           tabelaAddLivro.setValueAt(txtEditora.getText(), tabelaAddLivro.getSelectedRow(), 2);
+        }else{
+            JOptionPane.showMessageDialog(null, "Selecione Um Livro para Editar ");
+            
+        }
+    }//GEN-LAST:event_jButton3ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
