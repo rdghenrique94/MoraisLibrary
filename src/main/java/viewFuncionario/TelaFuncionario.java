019f6205c1adb0f33a29d731b213c277bc3edfe2
Marlysonn <marlysonnxavier@gmail.com>
2020-11-30 20:01:54 -0300

    Update Inclui Acervo

--- src/main/java/viewFuncionario/TelaFuncionario.java
+++ src/main/java/viewFuncionario/TelaFuncionario.java
@@ -241,6 +241,7 @@ public class TelaFuncionario extends javax.swing.JFrame {
     private void jMenuItem9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem9ActionPerformed
     }//GEN-LAST:event_jMenuItem9ActionPerformed
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
+
         TelaDeleteUser tdu = new TelaDeleteUser();
         desktopFuncionario.add(tdu);
         tdu.setVisible(true);
